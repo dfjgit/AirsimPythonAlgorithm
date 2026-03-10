d1b4f803ed43350f5a9297a492d3be099beee955
低复举 <297212560@qq.com>
2026-03-10 23:36:09 +0800

    调整 AirSim 虚拟训练时长为 12 小时

--- multirotor/DDPG_Weight/run_training.bat
+++ multirotor/DDPG_Weight/run_training.bat
@@ -1,5 +1,6 @@
-@echo off
+﻿@echo off
 chcp 65001 >nul 2>&1
 cd /d "%~dp0"
-python train_with_airsim_improved.py --config configs/unified_train_config.json --total-timesteps 14400
+python train_with_airsim_improved.py --config configs/unified_train_config.json --total-timesteps 21600
 pause
+
